4980bba52b2351d0f05fea702c41cbb8f219a20e
Adam Collins <adc613@gmail.com>
2019-05-19 15:32:14 -0700

    Add instructuors to connect to DB

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Project jarvis
+
+
+# Running Postgres
+
+```
+docker run --rm --name pg[jdocker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v ~/tmp/docker/volumes/postgres:/var/lib/postgresql/data postgres
+```
--- console/config/dev.exs
+++ console/config/dev.exs
@@ -69,7 +69,7 @@ config :phoenix, :plug_init_mode, :runtime
 # Configure your database
 config :console, Console.Repo,
   username: "postgres",
-  password: "postgres",
-  database: "console_dev",
+  password: "docker",
+  database: "postgres",
   hostname: "localhost",
   pool_size: 10
